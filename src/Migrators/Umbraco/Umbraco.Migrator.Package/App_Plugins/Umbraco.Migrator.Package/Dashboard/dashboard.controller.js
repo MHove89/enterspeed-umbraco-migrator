88ae1125e562fd3e0175a9b29e0869240a454f73
jesperweber <jweber@jweber.dk>
2024-05-21 10:35:41 +0200

    Update existing document types instead of manually have to delete them. This also mean that existing uSync export of document types can be reused as it's the same document type being updated.

--- src/Migrators/Umbraco/Umbraco.Migrator.Package/App_Plugins/Umbraco.Migrator.Package/Dashboard/dashboard.controller.js
+++ src/Migrators/Umbraco/Umbraco.Migrator.Package/App_Plugins/Umbraco.Migrator.Package/Dashboard/dashboard.controller.js
@@ -9,7 +9,7 @@
         migratorResource.importDocumentTypes().then(function (result) {
             notificationsService.success("Document types imported");
         }, function (error) {     
-            notificationsService.error("Something w ent wrong when importing  document types: " + error);
+            notificationsService.error("Something went wrong when importing  document types: " + error);
         });  
     }  
 
